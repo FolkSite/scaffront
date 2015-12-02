97f98d0dbf7d80f4cb831fa282b5658af218880b
antixrist <ya.antixrist@gmail.com>
2015-12-02 18:09:50 +0400

    .

--- gulpfile.js/tasks/styles.js
+++ gulpfile.js/tasks/styles.js
@@ -49,6 +49,22 @@ gulp.task('sass:test', function (cb) {
 
 
 
+//// Provide `once: true` to restrict reloading to once per stream
+//gulp.task('templates', function () {
+//  return gulp.src('*.jade')
+//    .pipe(jade())
+//    .pipe(gulp.dest('app'))
+//    .pipe(bs.stream({once: true}));
+//});
+//
+//// Provide a filter to stop unwanted files from being reloaded
+//gulp.task('less', function () {
+//  return gulp.src('*.less')
+//    .pipe(less())
+//    .pipe(gulp.dest('css'))
+//    .pipe(bs.stream({match: "**/*.css"}));
+//});
+
 return;
 
 //gulpSass({
