d9690647e26f75d314c0cfb78949ed022f329b44
antixrist <ya.antixrist@gmail.com>
2015-12-08 18:11:59 +0400

    .

--- gulpfile.js/_config/images.js
+++ gulpfile.js/_config/images.js
@@ -1,9 +1,10 @@
-var _       = require('lodash'),
-    __      = require('../helpers'),
-    path    = require('path'),
-    extend  = require('extend'),
-    FS      = require('fs')
-;
+var _        = require('lodash'),
+    __       = require('../helpers'),
+    gulpUtil = require('gulp-util'),
+    path     = require('path'),
+    extend   = require('extend'),
+    FS       = require('fs')
+  ;
 
 
 var config = {};
@@ -24,6 +25,99 @@ config.dest = {
   content:            'dist/images'
 };
 
+config.transform = {
+  build: {
+    inline: function (stream) {
+      stream = stream
+        //.pipe()
+      ;
+
+      return stream;
+    },
+    inlineSprites: function (stream) {
+      stream = stream
+        //.pipe()
+      ;
+
+      return stream;
+    },
+    inlineSpritesSvg: function (stream) {
+      stream = stream
+        //.pipe()
+      ;
+
+      return stream;
+    },
+    libs: function (stream) {
+      stream = stream
+        //.pipe()
+      ;
+
+      return stream;
+    },
+    content: function (stream) {
+      stream = stream
+        //.pipe()
+      ;
+
+      return stream;
+    },
+  },
+  dist: {
+    inline: function (stream) {
+      stream = stream
+        //.pipe()
+      ;
+
+      return stream;
+    },
+    inlineSprites: function (stream) {
+      stream = stream
+        //.pipe()
+      ;
+
+      return stream;
+    },
+    inlineSpritesSvg: function (stream) {
+      stream = stream
+        //.pipe()
+      ;
+
+      return stream;
+    },
+    libs: function (stream) {
+      stream = stream
+        //.pipe()
+      ;
+
+      return stream;
+    },
+    content: function (stream) {
+      stream = stream
+        //.pipe()
+      ;
+
+      return stream;
+    },
+  },
+};
+
+config.cleanups = {
+  build: {
+    inline: __.getGlobPaths(config.dest, ['css', 'css.map', '!min.css', '!min.css.map'], true),
+    inlineSprites: '',
+    inlineSpritesSvg: '',
+    libs: '',
+    content: '',
+  },
+  dist: {
+    inline: '',
+    inlineSprites: '',
+    inlineSpritesSvg: '',
+    libs: '',
+    content: '',
+  }
+};
 
 /**
  * @type {Copier}
@@ -31,9 +125,9 @@ config.dest = {
  * @property {Copier} [css]
  */
 config.copier = [{
-  from: __.getGlobPaths(__.getBowerPath('fancybox/source'), ['gif', 'png', 'jpg', 'svg']),
-  to: path.join(config.src.libs, 'fancybox'),
-  cleanups: __.getGlobPaths(path.join(config.src.libs, 'fancybox'), ['gif', 'png', 'jpg', 'svg'])
+  //from: __.getGlobPaths(__.getBowerPath('fancybox/source'), ['gif', 'png', 'jpg', 'svg']),
+  //to: path.join(config.src.libs, 'fancybox'),
+  //cleanups: __.getGlobPaths(path.join(config.src.libs, 'fancybox'), ['gif', 'png', 'jpg', 'svg'])
 }];
 
 
