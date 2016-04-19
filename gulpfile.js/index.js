bd5c273d8f6c453db8fb56bfca05e6da1134ccea
Лёха <kartashov.al.and@yandex.ru>
2016-04-19 11:38:55 +0400

    .

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,24 +1,41 @@
-/**
- * @typedef {string|string[]} Glob
- */
+const gulp        = require('gulp');
+const gulpUtil    = require('gulp-util');
+const gulpInclude = require('gulp-include');
+const debug       = require('gulp-debug');
 
-/**
- * @callback StreamTransform
- * @param {Stream} stream
- * @param {Function} cb
- * @returns {Stream} stream
- */
+function lazyRequireTask(taskName, path, options) {
+  options = options || {};
+  options.taskName = taskName;
+  gulp.task(taskName, function(callback) {
+    let task = require(path).call(this, options);
 
-/**
- * @typedef  {{}}                           Copier
- * @property {Glob} from                    Source paths
- * @property {Glob} to                      Destination paths
- * @property {StreamTransform} [transform]  Stream transforms
- * @property {string|string[]} [cleanups]   Glob for removing files
- */
+    return task(callback);
+  });
+}
 
 
 
+var isProduction = (process.env.NODE_ENV === 'production');
+global.isProduction = isProduction;
+
+
+
+
+gulp.task('styles:css', function () {
+  gulp.src("src/js/main.js")
+    .pipe(gulpInclude())
+    .on('error', console.log)
+    .pipe(gulp.dest("dist/js"));
+});
+
+
+
+
+
+
+
+
+return;
 
 
 // https://makeomatic.ru/blog/2014/12/06/Tips_and_Tricks/
@@ -36,12 +53,6 @@
 
 
 //'use strict';
-
-global.isProduction = (process.env.NODE_ENV === 'production');
-global.environment = (global.isProduction) ? 'production' : 'development';
-
-global.isWatching = false;
-
 
 //var config = require('./_config/');
 //var Gulp = require('gulp');
@@ -921,187 +932,34 @@ gulp.task('watch', function (cb) {
 //  //                    if (new RegExp(match, 'gm').test(data)) {
 //  //                      data = data.replace(new RegExp('('+ match +')', 'gm'), '$1' + file.contents + '</style>');
 //  //                    } else {
-//  //                      data = data.replace(/([ \t]*)(<head[^>]*>)/, '$1$2\n$1  <style>/** critical css **/'+ file.contents + '</style>');
-//  //                    }
-//  //
-//  //                    FS.writeFile(htmlFilePath, data, (function (filename) {
-//  //                      return function(e) {
-//  //                        if (e) {
-//  //                          throw new Error(e);
-//  //                        }
-//  //                        console.log('Critical CSS injected to', filename);
-//  //                      };
-//  //                    })(filename));
-//  //                  }
-//  //
-//  //                })(htmlFilePath, filename, file)
-//  //            );
-//  //          });
-//  //        }))
-//  //        .pipe(gulpRename(function (path) {
-//  //          path.basename += '.min';
-//  //        }))
-//  //        .pipe(gulp.dest(paths.dist.criticalCss));
-//  //
-//  //  }, function (e) {
-//  //    unlinkTmpFile();
-//  //    handleError(e);
-//  //  });
-//  //}, function (e) {
-//  //  handleError(e);
-//  //});
-//
-//  //*/
-//});
-//Gulp.task('styles:critical', function (cb) {
-//  return RunSequence(
-//    'styles:critical:clean',
-//    'styles:critical:build',
-//    cb
-//  );
-//});
-//Gulp.task('styles:min', function () {
-//  return Gulp.src(paths.dist.css +'/**/*.css')
-//      .pipe(Plumber(Helpers.plumberErrorHandler))
-//      .pipe(Filter(function (file) {
-//        var is = Is(file);
-//        return is.css && !is.minified;
-//      }))
-//      .pipe(Autoprefixer(config.Autoprefixer))
-//      .pipe(Csso())
-//      .pipe(Rename(function (path) {
-//        if (!path.basename.match(/\.min$/)) {
-//          path.basename += '.min';
-//        }
-//      }))
-//      .pipe(Gulp.dest(paths.dist.css));
-//});
-//Gulp.task('styles:build', function (cb) {
-//  return RunSequence(
-//    ['sass:build', 'css:build'],
-//    cb
-//  );
-//});
-//Gulp.task('styles:dist', function(cb) {
-//  return RunSequence(
-//    'styles:build',
-//    //'styles:critical',
-//    //'styles:min',
-//    'styles:min',
-//    //'styles:critical',
-//    cb
-//  );
-//});
-//
-//Gulp.task('images:content:build', function () {
-//  return Gulp.src(paths.src.images.content)
-//    .pipe(Plumber(Helpers.plumberErrorHandler))
-//    .pipe(Cache('images:content:build'))
-//    .pipe(Gulp.dest(paths.dist.images.content));
-//});
-//Gulp.task('images:design:build', function () {
-//  return Gulp.src(paths.src.images.inline)
-//    .pipe(Plumber(Helpers.plumberErrorHandler))
-//    .pipe(Cache('images:inline:build'))
-//    .pipe(Gulp.dest(paths.dist.images.inline));
-//});
-//Gulp.task('images:build', function (cb) {
-//  return RunSequence(['images:content:build', 'images:inline:build'], cb);
-//});
-//Gulp.task('images:design:min', function () {
-//  return Gulp.src(paths.dist.images.inline +'**/*.*')
-//    .pipe(Plumber(Helpers.plumberErrorHandler))
-//    .pipe(ImageMin(config.ImageMin))
-//    .pipe(Gulp.dest(paths.dist.images.inline));
-//});
-//Gulp.task('images:content:min', function () {
-//  return Gulp.src(paths.dist.images.content)
-//    .pipe(Plumber(Helpers.plumberErrorHandler))
-//    .pipe(ImageMin(config.ImageMin))
-//    .pipe(Gulp.dest(paths.dist.images.content));
-//});
-//Gulp.task('images:min', function (cb) {
-//  return RunSequence(
-//    ['images:content:min', 'images:inline:min'],
-//    cb
-//  );
-//});
-//Gulp.task('images:design:dist', function (cb) {
-//  return RunSequence(
-//    'images:design:build',
-//    'images:design:min',
-//    cb
-//  );
-//});
-//Gulp.task('images:content:dist', function (cb) {
-//  return RunSequence(
-//    'images:content:build',
-//    'images:content:min',
-//    cb
-//  );
-//});
-//Gulp.task('images:dist', function (cb) {
-//  return RunSequence(
-//    ['images:content:dist', 'images:design:dist'],
-//    cb
-//  );
-//});
-//
-//Gulp.task('html:build', function () {
-//  return Gulp.src(paths.src.html)
-//    .pipe(Plumber(Helpers.plumberErrorHandler))
-//    .pipe(FileInclude(config.FileInclude))
-//    .pipe(Gulp.dest(paths.dist.html));
-//});
-//Gulp.task('html:dist', function (cb) {
-//  return RunSequence('html:build', cb);
-//});
-//
-//Gulp.task('build', function (cb) {
-//  RunSequence(
-//    ['removeDist', 'clearCache'],
-//    ['copyroot', 'fonts:build', 'js:build', 'images:build'],
-//    'styles:build', 'html:build',
-//    cb
-//  );
-//});
-//Gulp.task('dist', function (cb) {
-//  RunSequence(
-//    'removeDist',
-//    ['copyroot', 'html:dist', 'js:dist', 'images:dist', 'fonts:dist'],
-//    'styles:dist',
-//    cb
-//  );
-//});
-//
-//Gulp.task('server:start', function (cb) {
-//  return BrowserSync(config.BrowserSync, cb);
-//});
-//Gulp.task('server:stop', function () {
-//  return BrowserSync.exit();
-//});
-//Gulp.task('watch', ['server:start'], function () {
-//  Gulp.watch(paths.watch.images.inline, ['images:inline:build'], BrowserSync.reload);
-//  Gulp.watch(paths.watch.images.content, ['images:content:build'], BrowserSync.reload);
-//  Gulp.watch(paths.watch.css, ['css:build']
-//    //,BrowserSync.reload({stream: true})
-//  );
-//  Gulp.watch(paths.watch.sass, ['sass:build']
-//    //,BrowserSync.reload({stream: true})
-//  );
-//  Gulp.watch(paths.watch.js, ['js:build', BrowserSync.reload]);
-//  Gulp.watch(paths.watch.html, ['html:build', BrowserSync.reload]);
-//  Gulp.watch(paths.watch.root, ['copyroot', BrowserSync.reload]);
-//  Gulp.watch(paths.watch.fonts, ['fonts:build', BrowserSync.reload]);
-//});
-//
-//Gulp.task('default', function (cb) {
-//  RunSequence(
-//    'removeDist',
-//    ['copyroot', 'fonts:build', 'js:build', 'images:build', 'html:build'],
-//    'styles:build',
-//    ['server:start', 'watch'],
-//    cb
-//  );
-//});
-//
+//  //                      data = data.replace(/([ \t]*)(<head[^>]*>)/, '$1$2\n$1  <style>/** critical css **/'+
+// file.contents + '</style>'); //                    } // //                    FS.writeFile(htmlFilePath, data,
+// (function (filename) { //                      return function(e) { //                        if (e) { //
+//               throw new Error(e); //                        } //                        console.log('Critical CSS
+// injected to', filename); //                      }; //                    })(filename)); //                  } // //
+//                })(htmlFilePath, filename, file) //            ); //          }); //        })) //
+// .pipe(gulpRename(function (path) { //          path.basename += '.min'; //        })) //
+// .pipe(gulp.dest(paths.dist.criticalCss)); // //  }, function (e) { //    unlinkTmpFile(); //    handleError(e); //
+// }); //}, function (e) { //  handleError(e); //});  //*/ }); Gulp.task('styles:critical', function (cb) { return
+// RunSequence( 'styles:critical:clean', 'styles:critical:build', cb ); }); Gulp.task('styles:min', function () {
+// return Gulp.src(paths.dist.css +'/**/*.css') .pipe(Plumber(Helpers.plumberErrorHandler)) .pipe(Filter(function
+// (file) { var is = Is(file); return is.css && !is.minified; })) .pipe(Autoprefixer(config.Autoprefixer))
+// .pipe(Csso()) .pipe(Rename(function (path) { if (!path.basename.match(/\.min$/)) { path.basename += '.min'; } }))
+// .pipe(Gulp.dest(paths.dist.css)); }); Gulp.task('styles:build', function (cb) { return RunSequence( ['sass:build',
+// 'css:build'], cb ); }); Gulp.task('styles:dist', function(cb) { return RunSequence( 'styles:build',
+// //'styles:critical', //'styles:min', 'styles:min', //'styles:critical', cb ); });  Gulp.task('images:content:build',
+// function () { return Gulp.src(paths.src.images.content) .pipe(Plumber(Helpers.plumberErrorHandler))
+// .pipe(Cache('images:content:build')) .pipe(Gulp.dest(paths.dist.images.content)); });
+// Gulp.task('images:design:build', function () { return Gulp.src(paths.src.images.inline)
+// .pipe(Plumber(Helpers.plumberErrorHandler)) .pipe(Cache('images:inline:build'))
+// .pipe(Gulp.dest(paths.dist.images.inline)); }); Gulp.task('images:build', function (cb) { return
+// RunSequence(['images:content:build', 'images:inline:build'], cb); }); Gulp.task('images:design:min', function () {
+// return Gulp.src(paths.dist.images.inline +'**/*.*') .pipe(Plumber(Helpers.plumberErrorHandler))
+// .pipe(ImageMin(config.ImageMin)) .pipe(Gulp.dest(paths.dist.images.inline)); }); Gulp.task('images:content:min',
+// function () { return Gulp.src(paths.dist.images.content) .pipe(Plumber(Helpers.plumberErrorHandler))
+// .pipe(ImageMin(config.ImageMin)) .pipe(Gulp.dest(paths.dist.images.content)); }); Gulp.task('images:min', function
+// (cb) { return RunSequence( ['images:content:min', 'images:inline:min'], cb ); }); Gulp.task('images:design:dist',
+// function (cb) { return RunSequence( 'images:design:build', 'images:design:min', cb ); });
+// Gulp.task('images:content:dist', function (cb) { return RunSequence( 'images:content:build', 'images:content:min',
+// cb ); }); Gulp.task('images:dist', function (cb) { return RunSequence( ['images:content:dist',
+// 'images:design:dist'], cb ); });  Gulp.task('html:build', function () { return Gulp.src(paths.src.html) .pipe(Plumber(Helpers.plumberErrorHandler)) .pipe(FileInclude(config.FileInclude)) .pipe(Gulp.dest(paths.dist.html)); }); Gulp.task('html:dist', function (cb) { return RunSequence('html:build', cb); });  Gulp.task('build', function (cb) { RunSequence( ['removeDist', 'clearCache'], ['copyroot', 'fonts:build', 'js:build', 'images:build'], 'styles:build', 'html:build', cb ); }); Gulp.task('dist', function (cb) { RunSequence( 'removeDist', ['copyroot', 'html:dist', 'js:dist', 'images:dist', 'fonts:dist'], 'styles:dist', cb ); });  Gulp.task('server:start', function (cb) { return BrowserSync(config.BrowserSync, cb); }); Gulp.task('server:stop', function () { return BrowserSync.exit(); }); Gulp.task('watch', ['server:start'], function () { Gulp.watch(paths.watch.images.inline, ['images:inline:build'], BrowserSync.reload); Gulp.watch(paths.watch.images.content, ['images:content:build'], BrowserSync.reload); Gulp.watch(paths.watch.css, ['css:build'] //,BrowserSync.reload({stream: true}) ); Gulp.watch(paths.watch.sass, ['sass:build'] //,BrowserSync.reload({stream: true}) ); Gulp.watch(paths.watch.js, ['js:build', BrowserSync.reload]); Gulp.watch(paths.watch.html, ['html:build', BrowserSync.reload]); Gulp.watch(paths.watch.root, ['copyroot', BrowserSync.reload]); Gulp.watch(paths.watch.fonts, ['fonts:build', BrowserSync.reload]); });  Gulp.task('default', function (cb) { RunSequence( 'removeDist', ['copyroot', 'fonts:build', 'js:build', 'images:build', 'html:build'], 'styles:build', ['server:start', 'watch'], cb ); });
