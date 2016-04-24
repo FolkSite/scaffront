aafc953c32142ee6c947042bf88a5c6383572fc2
Kartashov Alexey <kartashov.al.and@yandex.ru>
2016-04-24 21:58:56 +0400

    .

--- webpack.config.js/index.js
+++ webpack.config.js/index.js
@@ -28,5 +28,16 @@ module.exports = {
 
       return _envs;
     }, {}))
-  ]
+  ],
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  }
 };
