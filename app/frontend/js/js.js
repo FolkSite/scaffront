1481f0d462c7fabd5022cd1c4d328869162598b4
Kartashov Alexey <kartashov.al.and@yandex.ru>
2016-04-24 21:24:17 +0400

    .

--- app/frontend/js/js.js
+++ app/frontend/js/js.js
@@ -3,7 +3,7 @@ import config from './config';
 console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 console.log('isProd', isProd);
 
-if (process.env.NODE_ENV == 'development') {
+if (isProd) {
   console.log('config in module', config);
 }
 
