d8e9b0b897a47a54fb4fb1f9e00b9c3a08dc4875
Kartashov Alexey <kartashov.al.and@yandex.ru>
2016-04-24 23:07:27 +0400

    .

--- app/frontend/js/js.js
+++ app/frontend/js/js.js
@@ -1,5 +1,8 @@
 import config from './config';
+import * as slice from 'sliced';
 //require('babel-runtime/core-js/promise').default = require('bluebird');
+
+console.log('slice', slice([1,2,3,4,5], 3));
 
 console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 console.log('isProd', isProd);
