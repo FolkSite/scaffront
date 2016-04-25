6deacfb7ecdbe8890fb1e848ed960b950cd9f2bd
Лёха <kartashov.al.and@yandex.ru>
2016-04-25 17:27:27 +0400

    .

--- webpack.config.js/index.js
+++ webpack.config.js/index.js
@@ -57,9 +57,9 @@ let config = {
 
   module: {
     loaders: [{
+      loader: 'babel',
       test: /\.js$/,
       exclude: /(node_modules|bower_components)/,
-      loader: 'babel',
       query: {
         presets: ['es2015'],
         plugins: [
