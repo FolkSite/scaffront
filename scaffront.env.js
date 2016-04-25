047c6e2ac2c48415858a13fa2ad31b5b0ed5f6f1
Лёха <kartashov.al.and@yandex.ru>
2016-04-25 19:18:03 +0400

    .

--- scaffront.env.js
+++ scaffront.env.js
@@ -21,13 +21,56 @@ config.paths.root = 'app/frontend';
 config.paths.dest = (config.isDev) ? 'dest/frontend/development' : 'dest/frontend/production';
 
 config.paths.scripts = {
-  src: __.glob(
-    path.join(config.paths.root, 'js'),
-    ['*.js', '!_*.js']
-  ),
+  src: __.glob(path.join(config.paths.root, 'js'), ['*.js', '!_*.js']),
   dest: path.join(config.paths.dest, 'js')
 };
 
-module.exports = config;
+config.paths.styles = {};
+config.paths.styles.css = {
+  src: __.glob(path.join(config.paths.root, 'css'), ['*.css', '!_*.css']),
+  dest: path.join(config.paths.dest, 'css'),
+  watch: __.glob(path.join(config.paths.root, 'css'), ['*.css'], true)
+};
+config.paths.styles.scss = {
+  src: __.glob(path.join(config.paths.root, 'css'), ['*.scss', '!_*.scss']),
+  dest: path.join(config.paths.dest, 'css'),
+  watch: __.glob(path.join(config.paths.root, 'css'), ['*.scss'], true)
+};
 
-console.log('config.paths', config.paths);
+
+config.servers = {
+  dev: {
+    ui: false,
+    open: false,
+    //reloadDelay: 1000,
+    //reloadDebounce: 1000,
+    ghostMode: false,
+
+    startPath: '/',
+    port: 1313,
+    server: {
+      index: 'index.html',
+      //directory: true,
+      baseDir: config.paths.dest
+    }
+  },
+  prod: {
+    ui: false,
+    open: false,
+    //reloadDelay: 1000,
+    //reloadDebounce: 1000,
+    ghostMode: false,
+
+    startPath: '/',
+    port: 13666,
+    server: {
+      index: 'index.html',
+      //directory: true,
+      baseDir: config.paths.dest
+    }
+  }
+};
+
+console.log('config', config);
+
+module.exports = config;
